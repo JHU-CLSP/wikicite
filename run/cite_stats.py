3be69ee7b321bce567ad8ff7095ef05ea3e0babf
Guanghui Qin <hiaoxui@gmail.com>
2024-07-15 12:44:34 -0400

    minor

--- run/cite_stats.py
+++ run/cite_stats.py
@@ -1,7 +1,8 @@
 from argparse import ArgumentParser
 from collections import defaultdict
+import random
 
-from tqdm import trange
+from tqdm import tqdm
 
 from wikicite.megawika_processor import MegaWika2Processor
 from wikicite.stats import describe_data
@@ -10,13 +11,23 @@ from wikicite.stats import describe_data
 def main():
     parser = ArgumentParser()
     parser.add_argument('--corpus', type=str)
-    parser.add_argument('--sample', type=int, default=10_000, help='#samples')
+    parser.add_argument('--sample', type=int, default=10_000, help='#samples/1000')
     args = parser.parse_args()
 
     mw = MegaWika2Processor(args.corpus, citation_keep_criterion='all')
+    thousands = list(range(len(mw.corpus)//1000))
+    random.shuffle(thousands)
+    thousands = thousands[:args.sample]
+
+    todos = []
+    for thousand in thousands:
+        for i in range(1000):
+            article_idx = thousand*1000 + i
+            if article_idx < len(mw.corpus):
+                todos.append(article_idx)
+
     stats = defaultdict(list)
-    n_article = min(len(mw.corpus), args.sample)
-    for article_idx in trange(n_article):
+    for article_idx in tqdm(todos):
         article = mw.process_article(article_idx)
         n_total = n_long = n_short = n_both = 0
         for para in article:

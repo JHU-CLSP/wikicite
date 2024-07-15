214c5c4a6d1f02931e9fdb32ae13ca19f12de0e3
Guanghui Qin <hiaoxui@gmail.com>
2024-07-15 12:33:13 -0400

    minor

--- wikicite/stats.py
+++ wikicite/stats.py
@@ -1,16 +1,26 @@
 from typing import Dict, List
+import math
 
 import numpy as np
 from tabulate import tabulate
+
+
+def significant_figures(x, digits=3):
+    if x == 0 or not math.isfinite(x):
+        return x
+    if math.log10(x) >= digits:
+        return x
+    digits -= math.ceil(math.log10(abs(x)))
+    return round(x, digits)
 
 
 def describe_data(count: Dict[str, List[int | float]]):
     quantiles = [0.05, 0.25, 0.5, 0.75, 0.95]
 
     def one_list(li):
-        nums = [round(np.mean(li), 1)]
+        nums = [significant_figures(float(np.mean(li)), 3)]
         for q in quantiles:
-            nums.append(round(np.quantile(li, q)))
+            nums.append(significant_figures(float(np.quantile(li, q)), 3))
         return nums
 
     headers = ['name', 'mean'] + [f'{q=}' for q in quantiles]

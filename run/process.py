e2c0d7c94825ccce70fa9853a0e681aebb4897d1
Guanghui Qin <hiaoxui@gmail.com>
2024-07-16 13:21:40 -0400

    1000 examples per file for dumper

--- run/process.py
+++ run/process.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from tqdm import tqdm
+from tqdm import trange
 
 from wikicite.megawika_processor import MegaWika2Processor
 from wikicite.dumper import Dumper
@@ -14,18 +14,13 @@ def main():
     args = parser.parse_args()
 
     processor = MegaWika2Processor(corpus_path=args.corpus, citation_keep_criterion=args.cite)
-    dumper = Dumper(lines_per_file=1000, dump_path=args.output, total=len(processor.corpus))
-    n_processed = dumper.check_processed()
-    print(f'Found {n_processed} already processed items. Skipping them.')
-    todos = dumper.get_todos()
+    dumper = Dumper(lines_per_file=1000, dump_path=args.output)
 
-    for article_idx in tqdm(todos):
-        dumper.put(idx=article_idx, items=processor.process_article(article_idx))
+    for article_idx in trange(len(processor.corpus)):
+        dumper.put(items=processor.process_article(article_idx))
+    dumper.dump(True)
 
-    if dumper.empty:
-        print('All done.')
-    else:
-        print('Warning: There might be files that are not dumped yet.')
+    print('All done.')
 
 
 if __name__ == '__main__':
--- wikicite/dumper.py
+++ wikicite/dumper.py
@@ -1,7 +1,4 @@
-from typing import *
 import os
-import math
-from collections import defaultdict
 try:
     import ujson as json
 except ImportError:
@@ -9,49 +6,22 @@ except ImportError:
 
 
 class Dumper:
-    def __init__(self, lines_per_file: int, dump_path: str, total: int):
-        self.lines_per_file, self.dump_path, self.total = lines_per_file, dump_path, total
-        self.total_files = math.ceil(total / lines_per_file)
-        self.last_file_lines = total % lines_per_file
-        self.processed = set()
-        self.cached = defaultdict(dict)
+    def __init__(self, lines_per_file: int, dump_path: str):
+        self.lines_per_file, self.dump_path = lines_per_file, dump_path
+        if os.path.exists(dump_path):
+            raise ValueError('Dump path exists.')
+        self.file_count = 0
+        self.cached = list()
 
-    def check_processed(self):
-        if not os.path.exists(self.dump_path):
-            return
-        for fn in os.listdir(self.dump_path):
-            if fn.endswith('jsonl'):
-                name = fn.split('.')[0]
-                self.processed.add(int(name))
-        return len(self.processed) * self.lines_per_file
+    def put(self, items: list):
+        self.cached.extend(items)
+        self.dump(False)
 
-    def get_todos(self) -> List[int]:
-        ret = list()
-        for i in range(self.total):
-            if i // self.lines_per_file not in self.processed:
-                ret.append(i)
-        return ret
-
-    def put(self, idx: int, items: list):
-        file_idx = idx // self.lines_per_file
-        list_idx = idx % self.lines_per_file
-        self.cached[file_idx][list_idx] = [json.dumps(item) for item in items]
-
-        if self.need_dump(file_idx):
+    def dump(self, last: bool):
+        while len(self.cached) >= (self.lines_per_file if not last else 1):
             os.makedirs(self.dump_path, exist_ok=True)
-            dikt = self.cached.pop(file_idx)
-            lines = []
-            for k in sorted(list(dikt.keys())):
-                lines.extend(dikt[k])
-            with open(os.path.join(self.dump_path, f'{file_idx:05}.jsonl'), 'w') as fp:
-                fp.write('\n'.join(lines))
-
-    def need_dump(self, file_idx: int) -> bool:
-        if file_idx == self.total_files - 1:
-            return len(self.cached[file_idx]) >= self.last_file_lines
-        else:
-            return len(self.cached[file_idx]) >= self.lines_per_file
-
-    @property
-    def empty(self) -> bool:
-        return len(self.cached) == 0
+            to_dump = list(map(json.dumps, self.cached[:self.lines_per_file]))
+            self.cached = self.cached[self.lines_per_file:]
+            with open(os.path.join(self.dump_path, f'{self.file_count:06}.jsonl'), 'w') as fp:
+                fp.write('\n'.join(to_dump))
+            self.file_count += 1
